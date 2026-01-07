ae175e863316e35017b748276eaff0786e2aee7d
Veronika <sveronikasveronika01@gmail.com>
2026-01-07 13:26:00 +0200

    Update 02_insert_data.sql

--- production_analitics/02_insert_data.sql
+++ production_analitics/02_insert_data.sql
@@ -1,10 +1,20 @@
 -- insert initial production data
 
-INSERT INTO production(production_date, product_name, planned_quantity, actual_quantity) 
+INSERT INTO production (production_date, planned_quantity, actual_quantity, prod_id)
 VALUES
-('2025-01-10', 'Product A', 50, 40),
-('2025-08-25', 'Product B', 120, 100),
-('2025-10-04', 'Product C', 100, 99),
-('2025-08-30', 'Product D', 220, 190),
-('2025-05-19', 'Product E', 80, 80),
-('2025-08-28', 'Product F', 156, 149);
+('2025-01-10', 50, 40, 1),  
+('2025-08-25', 120, 100, 2), 
+('2025-10-04', 100, 99, 3), 
+('2025-08-30', 220, 190, 2), 
+('2025-05-19', 80, 80, 4),   
+('2025-08-28', 156, 149, 5); 
+
+-- insert initial products data
+
+INSERT INTO products(prod_name, category, unit)
+VALUES
+('TV', 'electronics', 'pcs'),
+('Sofa', 'furniture', 'pcs'),
+('Earrings', 'accesories', 'pcs'),
+('Table', 'furniture', 'pcs'),
+('Braclet', 'accesories', 'pcs');
