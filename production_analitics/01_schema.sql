93d40171e095c1c3e29d608bee636af3c19e1745
Veronika <sveronikasveronika01@gmail.com>
2026-01-07 13:25:11 +0200

    Update 01_schema.sql

--- production_analitics/01_schema.sql
+++ production_analitics/01_schema.sql
@@ -1,9 +1,17 @@
 -- schema for production analytics project
+CREATE TABLE products (
+    prod_id SERIAL PRIMARY KEY,
+    prod_name TEXT NOT NULL,
+    category TEXT,
+    unit TEXT
+);
 
+-- schema for products 
 CREATE TABLE production (
     id SERIAL PRIMARY KEY,
     production_date DATE,
-    product_name TEXT,
     planned_quantity INT,
-    actual_quantity INT
+    actual_quantity INT,
+    prod_id INT REFERENCES products(prod_id)
 );
+
