c1855e9874f84c50a92b0650edd11805218f6b12
Veronika <sveronikasveronika01@gmail.com>
2026-01-08 14:24:07 +0200

    Update 03_analysis.sql

--- production_analitics/03_analysis.sql
+++ production_analitics/03_analysis.sql
@@ -11,4 +11,20 @@ WHERE product_name LIKE '%D%';
 
 -- Between 150 & 220
 SELECT product_name FROM production
+
 WHERE planned_quantity BETWEEN 150 AND 220;
+
+-- join tables
+
+SELECT 
+	p.production_date,
+	p.planned_quantity,
+	p.actual_quantity,
+	pr.prod_id,
+	pr.prod_name,
+	pr.category,
+	pr.unit
+	
+FROM production AS p
+JOIN products AS pr
+ON p.prod_id = pr.prod_id;
